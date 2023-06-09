43d5d9ceecdf5579f40cbb8c5ebc6aeaa7fca63b
UntillNIGHT <lengleng9090@gmail.com>
2023-06-09 23:00:29 +0700

    [ADD] JPA Entities

---
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -0,0 +1,20 @@
+package guru.springframework.domain;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private String description;
+    private BigDecimal amount;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
+
+    @ManyToOne
+    private Recipe recipe;
+}
